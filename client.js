765bcaf17a5daade0575b37e8e656cecdd0b0371
Rohan Bisht <bishtrohan33@gmail.com>
2025-03-25 14:11:09 +0530

    basic networking

--- client.js
+++ client.js
@@ -3,7 +3,7 @@ const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 400;
 
-const socket = io('https://stickman-fighter.onrender.com');
+const socket = io(); // Connect to server
 
 let players = {};
 let localPlayer = null;
